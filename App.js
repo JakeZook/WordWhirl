0eda97b73c78b7c7cb4ee277b98d8add29c65718
Jake Zook <jakezook01@gmail.com>
2024-03-24 13:38:08 -0600

    Added game board components and styles.

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, SafeAreaView, View, ScrollView } from "react-native";
 
 import { colors } from "./src/constants";
 import Keyboard from "./src/components/Keyboard";
 
+const NUM_TRIES = 6;
+
 export default function App() {
+	const word = "hello";
+	const letters = word.split("");
+
+	const rows = Array.from({ length: NUM_TRIES }, (_, rowIndex) =>
+		rowIndex === 0 ? letters : new Array(letters.length).fill("")
+	);
+
 	return (
 		<SafeAreaView style={styles.container}>
 			<StatusBar style="light" />
 
-			<Text style={styles.title}>Word Whirl</Text>
+			<Text>
+				<Text style={styles.title}>Word </Text>
+				<Text style={[styles.title, styles.titleSecondary]}>Whirl</Text>
+			</Text>
+
+			<ScrollView style={styles.map}>
+				{rows.map((row, rowIndex) => (
+					<View key={`row-${rowIndex}`} style={styles.row}>
+						{row.map((cell, cellIndex) => (
+							<View key={`cell-${rowIndex}-${cellIndex}`} style={styles.cell}>
+								<Text style={styles.cellText}>{cell.toUpperCase()}</Text>
+							</View>
+						))}
+					</View>
+				))}
+			</ScrollView>
 
 			<Keyboard />
 		</SafeAreaView>
@@ -23,8 +47,37 @@ const styles = StyleSheet.create({
 		alignItems: "center",
 	},
 	title: {
-		color: colors.white,
+		color: colors.primary,
 		fontSize: 32,
 		fontWeight: "bold",
+		letterSpacing: 5,
+	},
+	titleSecondary: {
+		color: colors.secondary,
+	},
+	map: {
+		alignSelf: "stretch",
+		height: 100,
+		marginVertical: 20,
+	},
+	row: {
+		alignSelf: "stretch",
+		flexDirection: "row",
+	},
+	cell: {
+		margin: 3,
+		flex: 1,
+		aspectRatio: 1,
+
+		justifyContent: "center",
+		alignItems: "center",
+
+		borderWidth: 1,
+		borderColor: colors.lightgrey,
+	},
+	cellText: {
+		color: colors.white,
+		fontWeight: "bold",
+		fontSize: 28,
 	},
 });
